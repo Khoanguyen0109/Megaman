9ed71e1c6a990eb678e71fb6eca45a2c4349e848
= <Khoanguyen0109.99@gmail.com>
2020-03-20 00:20:50 +0700

    megaman 3.10

--- src/com/rock/gameobjects/PhysicalMap.java
+++ src/com/rock/gameobjects/PhysicalMap.java
@@ -38,7 +38,7 @@ public class PhysicalMap extends GameObject {
         this.tileSize = tileSize;
     }
     
-    public void draw (Graphics2D g2){
+    public void draw (Graphics2D g2){ 
         
         g2.setColor(Color.gray);
          for(int i = 0;i< phys_map.length;i++)
@@ -54,17 +54,17 @@ public class PhysicalMap extends GameObject {
         int posX1 = rect.x/tileSize; /// index cua hien tai cua nhan vat tren mảng  
                                      /// chổ bắt đầu để duyệt mảng va chạm 
                                      /// vị trí góc trên bên trái của nhân vật
-        posX1 -=2;
+        posX1 -=2;// tăng vùng kiểm tra va chạm
         int posX2 = (rect.x + rect.width )/ tileSize;  // vị trí x gốc trên bên phải của nhân vật 
-        posX2 +=2;
+        posX2 +=2;// tăng vùng kiểm tra va chạm
         
         int posY2 =(rect.y + rect.height)/ tileSize;
         
-        if(posX1 < 0){
+        if(posX1 < 0){ 
             posX1 =0 ;
         }
         if( posX2 >= phys_map[0].length){
-            posX2 = phys_map[0].length-1;
+            posX2 = phys_map[0].length-1; //set vị trí nhân vật chạm mặt đất
         }
         for (int y = posY2;y<phys_map.length;y++){
             for (int x = posX1; x<= posX2;x++){
@@ -80,9 +80,11 @@ public class PhysicalMap extends GameObject {
     }
     
     public Rectangle haveCollisionWithTop (Rectangle rect){ // va cham với trần 
-        
-        int posX1 = rect.x/tileSize;
-        posX1 -=2;
+        /// kiểm tra xem hình chữ nhật của nhân vật có va chạm với trần hya không
+        int posX1 = rect.x/tileSize; /// index cua hien tai cua nhan vat tren mảng  
+                                     /// chổ bắt đầu để duyệt mảng va chạm 
+                                     /// vị trí góc trên bên trái của nhân vật  
+        posX1 -=2; // trừ 2 index của mảng
         int posX2 = (rect.x+ rect.width)/tileSize;
         posX2 +=2;
         
@@ -93,7 +95,7 @@ public class PhysicalMap extends GameObject {
         if (posX2 >= phys_map[0].length )
             posX2 =phys_map[0].length-1;
         
-        for ( int y = posY; y>=0; y-- ){
+        for ( int y = posY; y>=0; y-- ){// duyệt trong phạm vi nhân vật 
             for ( int x =posX1 ; x <= posX2; x++){
                 if ( phys_map[y][x] == 1){
                     Rectangle r = new Rectangle((int)getPosX() + x *tileSize, (int) getPosY() +y *tileSize,tileSize,tileSize);
